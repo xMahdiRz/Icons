ac4d8d69fdb207b17eecde3bf9cd19396e06b45b
Mahdi <mehdi.rezaiguia9@gmail.com>
2024-06-09 01:39:57 +0100

    correcting path

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,9 @@
 "use client";
 import { useState, useEffect } from "react";
 import iconsData from "./icons.json";
-import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/Card";
-import { Button } from "@/components/ui/Button";
-import { Input } from "@/components/ui/Input";
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import {
   Select,
   SelectValue,
